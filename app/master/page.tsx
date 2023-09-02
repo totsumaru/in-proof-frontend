cbc2c739b925a52aca73a9e84822088e8c442493
totsuka <techstart35@gmail.com>
2023-09-03 08:58:16 +0900

    ページを作成

---
+++ app/master/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import MasterHeader from "@/components/header/MasterHeader";
+
+export default function Master() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <MasterHeader/>
+      <div className="mx-auto max-w-7xl px-4 pt-5 sm:px-6 lg:px-8">
+        <h1 className="text-2xl text-gray-900 font-bold">Masterダッシュボード</h1>
+
+        {/* 設定 */}
+        <div className="mt-5">
+        </div>
+      </div>
+    </div>
+  )
+}
